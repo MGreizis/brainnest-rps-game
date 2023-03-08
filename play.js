833aaa458d7bc1e6f5ec45a7d293ff91551aa9ab
Jasline <jassy11992@gmail.com>
2023-03-07 19:35:55 -0500

    Looked up how to finish the coding for compPlay
    lmk if this works!

--- play.js
+++ play.js
@@ -13,5 +13,29 @@ function isChoice(word) {
   return selection.includes(word);
 }
 
+function getDecision() {
+  let decision;
+  while (true) {
+    decision = prompt("Rock, Paper, or Scissors?")
+      .toLowerCase()
+      .trim();
+    if (isChoice(decision)) break;
+    alert(
+      `Input "${decision}"\ invalid Please enter: rock, paper, or scissors`
+    );
+    }
+
+  switch (decision) {
+    case ROCK:
+      return ROCK;
+    case PAPER:
+      return PAPER;
+    case SCISSORS:
+      return SCISSORS;
+    default:
+      console.error("Unknown case");
+  }
+}
+
 
 compPlay();
