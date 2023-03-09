3cea33cb49c1824566f57294a41e4b2ad3deed66
DRGWeiss <ruizd1710@gmail.com>
2023-03-09 11:15:24 -0500

    cancel game option + code fixes and comments

--- game.js
+++ game.js
@@ -1,63 +1,87 @@
 let playerScore = 0;
 let computerScore = 0;
+let gameCancelled = false;
 
 function gameController(getPlayerInput, getComputerInput, getRoundWinner) {
-  const numberOfRounds = askRounds()
+  // Initialize the scores for a new game
+  playerScore = 0;
+  computerScore = 0;
+  gameCancelled = false
+  // Play game
+  const numberOfRounds = askTotalRounds();
   for (let i = 0; i < numberOfRounds; i++) {
-    round(getPlayerInput, getComputerInput, getRoundWinner)
+    if (!gameCancelled) {
+      round(getPlayerInput, getComputerInput, getRoundWinner, i + 1);
+    } else {
+      return
+    }
   }
+  // Declare set winner after all rounds
   if (playerScore > computerScore) {
-    alert('Congratulations. You Win!')
+    alert('Congratulations. You Win!');
   }
   else if (computerScore > playerScore) {
-    alert('Bad luck. Opponent Wins')
+    alert('Bad luck. Opponent Wins');
   }
   else {
-    alert('It\'s a draw')
+    alert('It\'s a draw');
+  }
+  // Play again option
+  const playAgain = prompt('Play Again? Type Yes(Y) or No(N)').toLowerCase()
+  if (playAgain === 'yes' || playAgain === 'y') {
+    return gameController(getPlayerInput, getComputerInput, getRoundWinner)
   }
 }
 
-function askRounds() {
-  const roundsRegExp = /^([5-9]|1[0-9]|20)$/
-  let numberOfRounds = prompt('How many rounds do you wish to play?')
+function cancelGame() {
+  gameCancelled = true
+  alert('The game has been cancelled')
+}
+
+function askTotalRounds() {
+  const roundsRegExp = /^([5-9]|1[0-9]|20)$/;
+  let numberOfRounds = prompt('How many rounds do you wish to play?');
   while (!roundsRegExp.test(numberOfRounds)) {
-     numberOfRounds = prompt('Please input a number from 5 to 20. How many rounds do you wish to play?')
+     numberOfRounds = prompt('Please input a number from 5 to 20. How many rounds do you wish to play?');
   }
-  console.log(`Winner of ${numberOfRounds} rounds`)
-  return Number(numberOfRounds)
+  console.log(`Winner of ${numberOfRounds} rounds`);
+  return Number(numberOfRounds);
 }
 
-function round(getPlayerInput, getComputerInput, roundWinner) {
-  const computerInput = getComputerInput()
-  const playerInput = getPlayerInput()
-  const winner = roundWinner(playerInput, computerInput)
+function round(getPlayerInput, getComputerInput, roundWinner, round) {
+  const computerInput = getComputerInput();
+  const playerInput = getPlayerInput(round);
+  // check cancel option
+  if (!playerInput) {
+    return
+  }
+  console.log(`You chose ${playerInput}`);
+  console.log(`Opponent chose ${computerInput}`);
+  const winner = roundWinner(playerInput, computerInput);
   switch (winner) {
     case 'player': {
-      playerScore++
-      console.log('You win this round')
+      playerScore++;
+      console.log('You win this round');
       break;
     }
     case 'computer': {
-      computerScore++
-      console.log('Opponent wins this round')
+      computerScore++;
+      console.log('Opponent wins this round');
       break;
     }
     default : {
-      console.log('It \'s a draw')
+      console.log('It \'s a draw');
     }
       
   }
-  console.log(`Current Score: Player ${playerScore} - ${computerScore} Opponent`)
+  console.log(`Current Score: Player ${playerScore} - ${computerScore} Opponent`);
 }
 
-// winner ??
 const roundWinner = (playerInput, computerInput) => {
   let s = "scissors";
   let p = "paper";
   let r = "rock";
   let result = "";
-
-  console.log(roundWinner);
   if (
     (playerInput == p && computerInput == r) ||
     (playerInput == s && computerInput == p) ||
@@ -73,10 +97,8 @@ const roundWinner = (playerInput, computerInput) => {
   } else {
     result = "draw";
   }
-  console.log(result)
   return result;
 };
-
 
 function compPlay() {
   const options = ["rock", "paper", "scissors"];
@@ -84,8 +106,26 @@ function compPlay() {
   return options[primary];
 }
 
-function getPlayerInput() {
-  let input = prompt("Enter rock, paper, or scissors:").toLowerCase();
+function getPlayerInput(round) {
+  let input = prompt(`Round ${round}. Enter rock, paper, or scissors:`)
+  // Users types something
+  if (typeof input === 'string') {
+    input = input.toLowerCase()
+  }
+  // If the user hits cancel instead
+  else {
+    const exit = confirm('Exit the game?')
+    if (exit) {
+      return cancelGame()
+    } else {
+      return getPlayerInput(round)
+    }
+  }
+  // Possible grammar errors
+  if (input === 'scisors' || input === 'scisor') {
+    input = 'scissors';
+  }
+  // Check correct input
   while (input !== "rock" && input !== "paper" && input !== "scissors") {
     input = prompt(
       "Invalid input. Please enter rock, paper, or scissors:"
@@ -94,6 +134,4 @@ function getPlayerInput() {
   return input;
 }
 
-
-// rps("paper", "scissors");
 gameController(getPlayerInput, compPlay, roundWinner);
